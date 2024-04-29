4a4b42c4dfabc653ceab61dd8debe64b85e59312
Michal Konik <---->
2024-04-29 23:44:50 +0200

    feature/migrations - adding table to db code

--- src/Service/SchemaBuilder.php
+++ src/Service/SchemaBuilder.php
@@ -4,14 +4,22 @@ namespace Mkt\MoneyPattern\Service;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Schema\Exception\TableAlreadyExists;
 use Doctrine\DBAL\Schema\Schema;
 
 class SchemaBuilder
 {
 
+    /**
+     * @throws Exception
+     */
     public function createSchema(Connection $connection)
     {
         $schema = new Schema();
+        if ($schema->hasTable('ExchangeRate')) {
+            throw new TableAlreadyExists('Tale ExchangeRate alreadyExists');
+        }
+
         $table = $schema->createTable('ExchangeRate');
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('source_currency', 'string', ['notnull' => true]);
@@ -19,14 +27,11 @@ class SchemaBuilder
         $table->addColumn('source_currency', 'float', ['notnull' => true]);
         $table->addColumn('last_update', 'datetime', ['notnull' => true]);
         $table->setPrimaryKey(['id']);
+        $queries = $schema->toSql($connection->getDatabasePlatform());
 
-        try {
-            $queries = $schema->toSql($connection->getDatabasePlatform());
-            foreach ($queries as $query) {
-                $connection->executeQuery($query);
-            }
-        } catch(Exception $e) {
-
+        foreach ($queries as $query) {
+            $connection->executeQuery($query);
         }
+
     }
 }
