bbc61c8c46708b2ff1785eb6d674977a569c2d89
Michal Konik <---->
2024-04-29 23:59:40 +0200

    feature/migrations - adding table to db code

--- src/Service/SchemaBuilder.php
+++ src/Service/SchemaBuilder.php
@@ -9,29 +9,48 @@ use Doctrine\DBAL\Schema\Schema;
 
 class SchemaBuilder
 {
+    private const TABLE_NAME = 'ExchangeRate';
+
+    public function __construct(private Connection $connection)
+    {
+    }
 
     /**
      * @throws Exception
      */
-    public function createSchema(Connection $connection)
+    public function createTable()
     {
         $schema = new Schema();
-        if ($schema->hasTable('ExchangeRate')) {
+        if ($schema->hasTable(self::TABLE_NAME)) {
             throw new TableAlreadyExists('Tale ExchangeRate alreadyExists');
         }
 
-        $table = $schema->createTable('ExchangeRate');
+        $table = $schema->createTable(self::TABLE_NAME);
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('source_currency', 'string', ['notnull' => true]);
         $table->addColumn('target_currency', 'string', ['notnull' => true]);
         $table->addColumn('source_currency', 'float', ['notnull' => true]);
         $table->addColumn('last_update', 'datetime', ['notnull' => true]);
         $table->setPrimaryKey(['id']);
-        $queries = $schema->toSql($connection->getDatabasePlatform());
+        $queries = $schema->toSql($this->connection->getDatabasePlatform());
 
         foreach ($queries as $query) {
-            $connection->executeQuery($query);
+            $this->connection->executeQuery($query);
         }
 
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function differentTableSameNameExists(): bool
+    {
+        $schemaManager = $this->connection->createSchemaManager();
+
+        if (!$schemaManager->tableExists(self::TABLE_NAME)) {
+            return false;
+        }
+        $tableColumns = $schemaManager->listTableColumns(self::TABLE_NAME);
+        return true;
     }
 }
