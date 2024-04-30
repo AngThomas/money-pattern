a0256f37e6dc05dfc9a2ab279012aa55f01ed2e8
Michal Konik <---->
2024-05-01 01:26:36 +0200

    feature/migrations - adding table to db code

--- src/Service/MoneyCalculator.php
+++ src/Service/MoneyCalculator.php
@@ -2,10 +2,29 @@
 
 namespace Mkt\MoneyPattern\Service;
 
+use Mkt\MoneyPattern\VO\Money;
+
 class MoneyCalculator
 {
-    public function add()
+    public function __construct(private CurrencyConverter $currencyConverter)
     {
+    }
 
+    public function add(Money $money, Money $moneyToAdd): Money
+    {
+        if ($money->getCurrency() === $moneyToAdd->getCurrency()) {
+            return $money->add($moneyToAdd);
+        }
+        $convertedMoneyToAdd = $this->currencyConverter->convertToCurrency($moneyToAdd, $money->getCurrency());
+        return $money->add($convertedMoneyToAdd);
+    }
+
+    public function substract(Money $money, Money $moneyToSubstract)
+    {
+        if ($money->getCurrency() === $moneyToSubstract->getCurrency()) {
+            return $money->substract($moneyToSubstract);
+        }
+        $convertedMoneyToSubstract = $this->currencyConverter->convertToCurrency($moneyToSubstract, $money->getCurrency());
+        return $money->substract($convertedMoneyToSubstract);
     }
 }
